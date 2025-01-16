40fb6797b4bdb59c9258310c55eba1968c0400fc
OlolaJaco <akinbusolaakinola2@gmail.com>
2025-01-16 23:30:16 +0100

    fixed a bug

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,6 @@ import validation from '../assets/validation.png';
 import chatApp from '../assets/chat app.png';
 import dashboard from '../assets/dashboard.png';
 import shareduled from '../assets/shareduled.png';
-import { text } from 'motion/react-client';
 
 export const Hero_Content: string = `A detail-oriented web developer with 1+ years experience in creating dynamic and
 responsive web applications. Driven by a client-first mindset and result oriented
